731f97bae3ff0a124167e06a501bc5201d3ac89f
James Harris <computersherpa@gmail.com>
2017-04-23 18:50:19 -0500

    Left-aligned scatterplot

--- script.js
+++ script.js
@@ -5,24 +5,37 @@ $.ajax('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
     var imgPath = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Poppy.png';
 
     var generateMoveSpeed = function() {
-        var moveSpeed = [];
+        var data = [];
+        var record = {};
         $.each(champions, function(champName, champObj) {
             var obj = {};
             obj.imgPath = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + champName + '.png';
             obj.moveSpeed = champObj.stats.movespeed * 10;
-            moveSpeed.push(obj);
+            
+            if (record[obj.moveSpeed] === undefined) {
+                record[obj.moveSpeed] = 0;
+            } else {
+                record[obj.moveSpeed] += 1;
+            }
+
+            obj.xOffset = record[obj.moveSpeed] * 50;
+
+            data.push(obj);
         });
-        return moveSpeed;
+        return data;
     }
-    moveSpeed = generateMoveSpeed();
+    data = generateMoveSpeed();
 
     d3.select('.graph')
       .selectAll('img')
-      .data(moveSpeed)
+      .data(data)
       .enter()
       .append('img')
         .attr('class', 'champ-icon')
-        .attr('src', function(d) { return d.imgPath } )
-        .attr('style', function(d) { return 'top: ' + (5000 - d.moveSpeed); } )
+        .attr('src', function(d) { return d.imgPath; } )
+        .attr('style', function(d) {
+            return 'bottom: ' + d.moveSpeed + 'px; left: ' + d.xOffset + 'px'
+        })
+        // .attr('style', function(d) { return 'bottom: ' + d.moveSpeed } )
     ;
 });
