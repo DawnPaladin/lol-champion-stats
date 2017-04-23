0bbbe0020f12bc2930236364e5ed578cb0c82477
James Harris <computersherpa@gmail.com>
2017-04-23 18:34:45 -0500

    Poorly-stacked scatterplot

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $.ajax('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json').done(function(response) {
     var champions = response.data;
     console.log(champions);
+
+    var imgPath = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Poppy.png';
+
+    var generateMoveSpeed = function() {
+        var moveSpeed = [];
+        $.each(champions, function(champName, champObj) {
+            var obj = {};
+            obj.imgPath = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + champName + '.png';
+            obj.moveSpeed = champObj.stats.movespeed * 10;
+            moveSpeed.push(obj);
+        });
+        return moveSpeed;
+    }
+    moveSpeed = generateMoveSpeed();
+
+    d3.select('.graph')
+      .selectAll('img')
+      .data(moveSpeed)
+      .enter()
+      .append('img')
+        .attr('class', 'champ-icon')
+        .attr('src', function(d) { return d.imgPath } )
+        .attr('style', function(d) { return 'top: ' + (5000 - d.moveSpeed); } )
+    ;
 });
