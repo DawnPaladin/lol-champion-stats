59081afea1f201f00e221ecc92b4985e1ee890e9
James Harris <computersherpa@gmail.com>
2017-04-30 19:48:15 -0500

    <img>s in <div>s

--- script.js
+++ script.js
@@ -2,47 +2,53 @@ $.ajax('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
     var champions = response.data;
     console.log(champions);
 
-    var spaceBetweenRows = 10;
-    var iconHeight = 50;
-    var numberOfRows = 7;
-
-    var yScale = d3.scaleLinear()
-        .domain([325, 355])
-        .range([(iconHeight + spaceBetweenRows) * numberOfRows, 0]);
-
     var generateMoveSpeed = function() {
-        var data = [];
-        var record = {};
+        var moveSpeeds = {};
 
         $.each(champions, function(champName, champObj) {
-
             var obj = {};
             obj.imgPath = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + champName + '.png';
-            obj.moveSpeed = yScale(champObj.stats.movespeed);
+            obj.name = champObj.name;
+            var currentSpeed = champObj.stats.movespeed;
+            obj.moveSpeed = currentSpeed;
             
-            if (record[obj.moveSpeed] === undefined) {
-                record[obj.moveSpeed] = 0;
+            if (moveSpeeds[currentSpeed] === undefined) {
+                moveSpeeds[currentSpeed] = [obj];
             } else {
-                record[obj.moveSpeed] += 1;
+                moveSpeeds[currentSpeed].push(obj);
             }
-            obj.xOffset = 40 + record[obj.moveSpeed] * 50;
-
-            data.push(obj);
+            obj.xOffset = 40 + moveSpeeds[currentSpeed].length * 50;
         });
-        return data;
+        return moveSpeeds;
     }
-    data = generateMoveSpeed();
+    moveSpeeds = generateMoveSpeed();
+    console.log(moveSpeeds);
 
     d3.select('.graph')
-      .selectAll('img')
-      .data(data)
-      .enter()
-      .append('img')
-        .attr('class', 'champ-icon')
-        .attr('src', function(d) { return d.imgPath; } )
-        .attr('style', function(d) {
-            return 'top: ' + d.moveSpeed + 'px; left: ' + d.xOffset + 'px'
+        .selectAll('.speedRow')
+        .data(d3.keys(moveSpeeds))
+        .enter()
+        .append('div').attr('class', 'speedRow').attr('data-speed', function(d) { return d; })
+        .sort(function(a, b) {
+            return a < b;
         })
     ;
+
+    $('.speedRow').each(function(index, row) {
+        var currentSpeed = $(row).data('speed');
+        var champs = moveSpeeds[currentSpeed];
+        d3.select(row)
+            .selectAll('img')
+            .data(champs)
+            .enter()
+            .append('img')
+                .attr('class', 'champ-icon')
+                .attr('src', function(d) { return d.imgPath; } )
+                .attr('style', function(d) {
+                    return 'top: ' + d.moveSpeed + 'px; left: ' + d.xOffset + 'px'
+                })
+                .attr('title', function(d) { return d.name });
+        ;
+    });
 
 });
